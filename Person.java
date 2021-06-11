7d15d94e842af89a3eec4145e90dcbefb3678570
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-11 12:15:44 +0530

    Added:Person.java

---
+++ Person.java
@@ -0,0 +1,42 @@
+package com.json;
+
+public class Person {
+	String firstName;
+	String lastName;
+	int age;
+	Address address;
+	String details;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	
+	
+}
